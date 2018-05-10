deaa00f44af2e75d4e6c412f99b46912eb7dae89
Famae Pascua <2143735@slu.edu.ph>
2018-05-10 20:41:41 +0800

    Fixed Bug

--- db.php
+++ db.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Koizumi Glo
- * Date: 10/05/2018
- * Time: 4:29 PM
- */
+
 
 $conn = new mysqli("localhost","root","","dbase");
 
--- logout.php
+++ logout.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Koizumi Glo
- * Date: 10/05/2018
- * Time: 4:22 PM
- */
+
 
 session_start();
 session_destroy();
